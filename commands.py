6c55a4800e7359ecd05261a1db45136110410a8e
Бен Аладжем <benialadjem@gmail.com>
2022-11-21 13:30:38 +0200

    Update commands.py

--- commands.py
+++ commands.py
@@ -149,23 +149,6 @@ class Gprs(Commands):
     # make dict{timestamp:[lat, lon, speed]}
     # convert ubinascii.b2a_base64(locJSON)
     # return GPSArray=
-    
-    def return_base64(res):
-        # res = [' 1', '1', '20221114101637.000', '42.675474', '23.289744', '621.057', '', '', '1', '', '500.0', '500.0', '500.0', '', '3', '', '5683.3', '189.4\r\n\r\nOK\r\n']
-        t = res[2].split(".")[0]
-        t = (int(t[0:4]), int(t[4:6]), int(t[6:8]), int(t[8:10]), int(t[10:12]), int(t[12:14]), 0, 0)
-
-        timestamp = str(utime.mktime(t) + 946684800)
-
-        loc_data = {}
-        loc_data[timestamp] = [float(res[3]), float(res[4]), float(res[5])]
-    #print(loc_data)
-
-        locJSON = json.dumps(loc_data)
-        loc64 = ubinascii.b2a_base64(locJSON)
-        loc64 = loc64.decode("utf-8")
-        loc64 = loc64.replace("\n", "")
-        return loc64
 
     def return_result(self):
         print("something")
