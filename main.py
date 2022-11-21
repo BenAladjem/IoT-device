fe156bcb460335f9e2c46f5a424b0b8bfa369893
Бен Аладжем <benialadjem@gmail.com>
2022-11-21 13:30:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ d = Database()
 
 f = FindyIoT()
 
+
+
 def beep():
     pwm0 = machine.PWM(machine.Pin(27))
     pwm0.duty(50)
@@ -23,8 +25,6 @@ if modem.isOn() == False:
     print("Turning on modem")
     modem.turnOn()
     beep()
-    #while not modem.isOn():
-        #utime.sleep(0.5)
 
 print("Check if modem is registered to network", modem.isReg())
 
@@ -37,14 +37,13 @@ if not d.read("200"):
     d.store()
 #ccid = modem.getCCID()
 batt = modem.getBat()[1]
-#batt = '66'
 
 latitude = 42.674884  # 254
 longitude = 23.289787  #255
 msl_altitude = 592.877  #256
-timestamp = 5  #257
+timestamp = 1669029794  #257
 speed = 44.2  #258
-gps_d = {"254": "42.674884", "255": "23.289787", "256": "592.877", "257": "5", "258": "44.2"}
+gps_d = {"254": "42.674884", "255": "23.289787", "256": "592.877", "257": "1669029794", "258": "44.2"}
 gps_def = {"254": "00", "255": "00", "256": "00", "257": "00", "258": "00"}
 value = d.read("565")
 print(value)
@@ -66,48 +65,8 @@ def write_report_type(r_type):
     d.write("565", r_type)
     d.store()
 
-'''
-def try_to_get_gps():
-    # only for database
-    count = 0
-    gps_dict = {"254":"", "255":"", "256": "", "257":"", "258":""}
-    if modem.isOnGPS() == "No":
-        return "Turn GPS ON"
-    while count < 20:
-        res = modem.getGPS()
-        if res == "No GPS":
-            utime.sleep(5)
-        else:
-            gps_dict["254"] = res[3]
-            gps_dict["255"] = res[4]
-            gps_dict["256"] = res[5]
-            gps_dict["257"] = res[2].split(".")[0]
-            gps_dict["258"] = res[6]
-            
-            return gps_dict
-        count += 1
-    return False
-
-def try_while_get_gps():
-    count = 0
-    while count < 5:
-        res = modem.getGPS()
-        if res == "No GPS":
-            utime.sleep(5)
-        else:
-            return res
-            
-        count += 1
-        
-        if count == 5:
-            return False
-
-'''
 
 def reading_command():
-    
-    #modem.turnOn()
-    #modem.connectHiGPS()
     
     com = modem.sendHiGPS("/input.php?IMEI=865456054799968").decode("utf-8")
     #com = modem.sendHiGPS("/input.php?IMEI=865456054799968&MSG=SLEEP-2;WORK-2;CYCLE-0;TRANS-3;OHR-1;INPUT-OPEN;&").decode("utf-8")
@@ -115,29 +74,27 @@ def reading_command():
     print("com= ", com)
     
 def send_GPS_to_the_server():
-    x = ''
+    gps = ''
     loc_info = ''
-    x = modem.gps()
+    gps = modem.gps()
+    imei = modem.getImei()
     batt = modem.getBat()#[1]
-    batt_descr = ",".join(batt) # returns str    
-    print("x = ",x)
-    if x != False:
-        #modem.turnOffGPS()
-        loc_info = modem.return_base64(x)
+    batt = ",".join(batt) # returns str    
+    print("gps = ",gps)
+    if gps != False:
+        loc_info = modem.return_base64(gps)
         #message = "/input.php?IMEI="+imei+"&bat="+batt+"&GPSArray="+loc_info+"=&"
-        message = "/input.php?IMEI="+imei+"&Description="+batt_descr+"&GPSArray="+loc_info+"=&"
+        message = "/input.php?IMEI="+imei+"&Description="+batt+"&GPSArray="+loc_info+"=&"
     else:
         # try to insert ENG
         name = "BeniTest"
         pas = "87654321"
-        imei = modem.getImei()
-        batt = modem.getBat()#[1]
-        batt = ",".join(batt) # returns str
-        cell_info = modem.parseCpsi()
-        description = name+imei+"BAT-"+batt+"GSM:0000,FFFF"+cell_info
+        #cell_info = modem.parseCpsi()
+        eng = modem.getEng()
+        description = name+imei+"BAT-"+batt+"GSM:0000,FFFF"+eng
         message = "/input.php?IMEI="+imei+"&User="+name+"&Pass="+pas+"&Description="+description
     
-    # IMEI=865456054799968&bat=77&GPSArray=WyI0Mi42NzQ4NjMiLCAiMjMuMjg5ODM1IiwgIjYwNC44NDciXQ=&
+    # IMEI=865456054799968&bat=77&GPSArray=WyI0Mi42NzQ4NjMiLCAiMjMuMjg5ODM1IiwgIjYwNC44NDcigpsQ=&
     #message = "/input.php?IMEI="+imei+"&bat="+batt+"&GPSArray="+loc_info+"=&"
     print("message = "+message)
     print("isOnGPS =",modem.isOnGPS())
@@ -161,21 +118,21 @@ def send_GPS_to_the_server():
 #/input.php?IMEI=865456054799968&bat=25&data=LTE;CAT-M1,OnlineOK=&
     
 #modem.sendUDP message:
-#/input.php?IMEI=865456054799968&bat=30&GPSArray=eyIxNjY4NjA0ODY4IjogWzQyLjY3NDc5LCAyMy4yODk4MywgNjQzLjMzNF19=&    
+#/input.php?IMEI=865456054799968&bat=30&GPSArray=eyIgpsNjY4NjA0ODY4IjogWzQyLjY3NDc5LCAyMy4yODk4MywgNjQzLjMzNF19=&    
    
-#  'LTE;CAT-M1,Online,284-05,0x0066,303617,339,EUTRAN-BAND3,1550,5,5,-10,-67,-44,15OK'
+#  'LTE;CAT-M1,Online,284-05,0gps0066,303617,339,EUTRAN-BAND3,1550,5,5,-10,-67,-44,15OK'
 
 def send_cpsi_to_the_server():
     name = "BeniTest"
     pas = "87654321"
-    cpsi = modem.getCPSI()
-    eng = modem.getEng()
+    #cpsi = modem.getCPSI()
+    #eng = modem.getEng()
     imei = modem.getImei()
     batt = modem.getBat()#[1]
     batt = ",".join(batt) # returns str
     cell_info = modem.parseCpsi()
-    print(type(cpsi),"   cpsi = ",cpsi)
-    print(type(eng), "   eng = ", eng)
+    #print(type(cpsi),"   cpsi = ",cpsi)
+    #print(type(eng), "   eng = ", eng)
     print(type(imei),"   imei = ",imei)
     print(type(batt), "   batt = ", batt)
     
@@ -212,7 +169,6 @@ def send_eng_to_the_server():
     if modem.isConnected():
         print("MODEM IS CONNECTED")
         modem.sendHiGPS(message)
-        #modem.cipClose()
     else:
         print("MODEM NOT CONNECTED")
     modem.cipClose()
