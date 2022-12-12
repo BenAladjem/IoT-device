e367aa483dcbe95b60828343db3f4530e7166696
Бен Аладжем <91260379+BenAladjem@users.noreply.github.com>
2022-12-12 23:01:37 +0200

    Update print_log.py

--- print_log.py
+++ print_log.py
@@ -1,37 +1,31 @@
-num_col = 5
-em =    "                |"
-em_row = [em]*len_row
-a = "aaaaaaa         |"
-names = [a]*len_row
+import config
 
-line =     "-----------------"
-arrow_r  = "-->             |"
-arrow_l  = "   <-------------"
-line_r  =  "   --------------"
-line_l  =  "---             |"
-row1 = [a, em, em, em, em, em]
-row2 = [em, a, em, em, em, em]
-row3 = [em, em, a, em, em, em]
-row4 = [em, em, em, a, em, em]
-row5 = [em, em, em, em, a, em]
-row6 = [em, em, em, em, em, a]
+log = config.log
 
-arrows={
-"arrow01" : [line_r,arrow_r,em,em ],
-"arrow02" : [line_r,line,arrow_r, em],
-"arrow03" : [line_r, line, line, arrow_r],
-"arrow12" : [em, line_r, arrow_r, em],
-"arrow13" : [em, line_r, line, arrow_r],
-"arrow23" : [em,em,line_r, arrow_r],
-"arrow10" : [arrow_l, line_l, em, em],
-"arrow20" : [arrow_l, line, line_l, em],
-"arrow30" : [arrow_l, line, line, line_l],
-"arrow21" : [em, arrow_l, line_l, em],
-"arrow31" : [em, arrow_l, line, line_l],
-"arrow32" : [em, em, arrow_l, line_l]
-}
-log = []
-log.append(names)
+num_col = config.num_col
+spase = config.spase
+class_names = config.class_names
+em_row = [spase]*num_col
+a = "  aaaaaaa         |"
+#names = [a]*num_col
+
+
+spase =    "                  |"
+line =     "-------------------"
+arrow_r  = "-->               |"
+arrow_l  = "    <--------------"
+line_r  =  "    ---------------"
+line_l  =  "---               |"
+row1 = [a, spase, spase, spase, spase, spase]
+row2 = [spase, a, spase, spase, spase, spase]
+row3 = [spase, spase, a, spase, spase, spase]
+row4 = [spase, spase, spase, a, spase, spase]
+row5 = [spase, spase, spase, spase, a, spase]
+row6 = [spase, spase, spase, spase, spase, a]
+
+
+
+#log.append(names)
 log.append(em_row)
 log.append(row1)
 log.append(row3)
@@ -70,10 +64,10 @@ def prt_log( log):
         p1 = 0
         p2 = 0
         for i in range(len(row)):
-            if row[i] != em:
+            if row[i] != spase:
                 p1 = i
         for j in range( len(row2)):
-            if row2[j] != em:
+            if row2[j] != spase:
                 p2 = j
         if p1 != p2 and r > 1:
             sr = "arrow"+str(p1)+str(p2)
@@ -89,7 +83,7 @@ def arrow_row(a, b):
     if a<b:
         for i in range(num_col):
             if i<a or i>b:
-                row.append(em)
+                row.append(spase)
             elif i>a and i < b:
                 row.append(line)
             elif i == a:
@@ -99,7 +93,7 @@ def arrow_row(a, b):
     elif a>b:
         for i in range(num_col):
             if i>a or i<b:
-                row.append(em)
+                row.append(spase)
             elif i<a and i > b:
                 row.append(line)
             elif i == a:
@@ -109,7 +103,7 @@ def arrow_row(a, b):
                             
     print("".join(row))
     
-def pr_log( log):
+def pr_log():
     for r in range(len(log)-1):
         row = log[r]
         row2 = log[r+1]
@@ -117,10 +111,10 @@ def pr_log( log):
         p1 = 0
         p2 = 0
         for i in range(num_col):
-            if row[i] != em:
+            if row[i] != spase:
                 p1 = i
         for j in range( num_col):
-            if row2[j] != em:
+            if row2[j] != spase:
                 p2 = j
         if p1 != p2 and r > 1:
             arrow_row(p1, p2)
@@ -136,7 +130,7 @@ for i in range(6):
     arrow_row(i, 0)
  '''   
 print()
-pr_log(log)
+pr_log()
 
 
     
